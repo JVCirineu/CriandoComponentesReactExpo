109ea0ff7a269b4e145ab54e7ca603273835a7e2
JVCirineu <132360356+JVCirineu@users.noreply.github.com>
2024-04-30 16:36:38 -0300

    facebook

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { styles } from '../css/style';
 import Botao from '../components/botao';
 import InputTexto from '../components/inputTexto';
@@ -10,15 +10,21 @@ export default function App() {
     console.log(newText);
   }
   return (
-    <View style={styles.container}>
-      <InputTexto label="Digite seu nome" dica="Digite seu nome"/>
-      <InputTexto valor={atualizaTexto} label="Digite seu CPF" dica="Digite seu CPF"/>
-      <InputTexto label="Digite sua senha" dica="Digite sua senha" seguranca={true}/>
+  
+  <View style={styles.container}>
+    <Image style={{width:'80%', height:80, marginBottom:30}}
+    resizeMode="contain"source={require('../../assets/facebook.png')}
+  />
 
-      <Botao btn="entrar" cor='#000' src='home' />
-      <Botao btn="excluir" cor='#f00' src='excluido' />
-      <Botao btn="cadastrar" cor='#0f0' src='cadastrar' />
-    </View>
+  <InputTexto label="Digite seu número ou email" dica="Digite seu número ou email"/>
+  <InputTexto label="Digite sua senha" dica="Digite sua senha" seguranca={true}/>
+
+  <Botao btn="entrar" cor='#1477F2' src='home' />
+  <View style={styles.cont}>
+    <View style={styles.line}/>
+    <Text style={styles.text}>ou</Text>
+    <View style={styles.line}/></View>
+    <Botao btn="criar uma nova conta" cor='' src='criar uma nova conta' />
+  </View>
   );
 }
-
--- src/css/botaoStyle.js
+++ src/css/botaoStyle.js
@@ -2,8 +2,8 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     button: {
-     paddingVertical: 20,
-     paddingHorizontal: 40,
+     paddingVertical: 12,
+     paddingHorizontal: 100,
      borderRadius: 5,
      marginVertical: 10,
     },
